597c61164f6452cda7082d3c7cb861fbce766b92
Lin He <he9lin@gmail.com>
2014-06-26 00:11:59 -0700

    Fix metadata

--- metadata.rb
+++ metadata.rb
@@ -6,5 +6,8 @@ description      'Installs/Configures Apache Ambari server and agents'
 long_description 'Installs/Configures Apache Ambari server and agents'
 version          '0.1.0'
 
+recipe "ambari::server", "Installs and configures Ambari server"
+recipe "ambari::agent",  "Installs and configures Ambari agent"
+
 depends 'yum',       '2.4.4'
 depends 'hostsfile', '2.4.5'
