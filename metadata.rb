460e4b97c438c7f15582752dddbd9c8429ecb3c8
Lin He <he9lin@gmail.com>
2014-06-29 18:19:44 -0700

    Pump up version

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'he9lin@gmail.com'
 license          'All rights reserved'
 description      'Installs/Configures Apache Ambari server and agents'
 long_description 'Installs/Configures Apache Ambari server and agents'
-version          '0.1.1'
+version          '0.1.2'
 
 recipe "ambari::server", "Installs and configures Ambari server"
 recipe "ambari::agent",  "Installs and configures Ambari agent"
