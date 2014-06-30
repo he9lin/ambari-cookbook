3692198000a0c4293edea76d21af814be4a6c646
Lin He <he9lin@gmail.com>
2014-06-30 07:57:58 -0700

    Stop iptables service

--- recipes/base.rb
+++ recipes/base.rb
@@ -6,6 +6,10 @@ service 'ntpd' do
   action [:enable, :start]
 end
 
+service 'iptables' do
+  action :stop
+end
+
 remote_file '/etc/yum.repos.d/ambari.repo' do
   source 'http://public-repo-1.hortonworks.com/ambari/centos6/1.x/updates/1.6.0/ambari.repo'
   action :create_if_missing
